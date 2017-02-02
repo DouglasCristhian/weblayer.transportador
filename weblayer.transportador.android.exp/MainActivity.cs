222e0cbb12132bd239c46fd64ca8e8ff44fe7e9f
DouglasCristhian (NoteBook DELL) <dc@weblayer.com.br>
2017-02-02 16:14:31 -0200

    --

---
+++ weblayer.transportador.android.exp/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace weblayer.transportador.android.exp
+{
+    [Activity(Label = "weblayer.transportador.android.exp", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
---
+++ weblayer.transportador.android.pro/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace weblayer.transportador.android.pro
+{
+    [Activity(Label = "weblayer.transportador.android.pro", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
